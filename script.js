39cc9dd2d3546ddd0a48f401be22023a0fdb6e12
Brandon Nguyen <rmpumaz@gmail.com>
2021-11-27 17:37:10 -0500

    created gamestart function and restart button linked

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Grab all elements to be interacted with 
+const boxElements = document.querySelectorAll(".box");
+const gameBoardElement = document.querySelector(".game-board");
+const restartButton = document.querySelector(".restartButton");
+
+//Make playerOne start as first
+let playerTwoTurn = false
+
+
+
 //Create Players for Game
 const playerOne = "x";
 const playerTwo = "o";
@@ -13,3 +23,17 @@ const winningStates = [
     [2,4,6],
 ]
 
+//Create a Start Game Function and for restart Button
+
+restartButton.addEventListener("click", gameStart)
+
+function gameStart(){
+    playerTwoTurn = false; // Default to playerOne Start as X
+    boxElements.forEach(box =>{
+        box.classList.remove(playerOne);//remove all previous players from last game
+        box.classList.remove(playerTwo);
+        box.removeEventListener("click", clickedBox); // remove previous clicked boxes 
+        box.addEventListener("click", clickedBox, {once: true}); // Makes sure the click event only happens once per click
+
+    })
+}
