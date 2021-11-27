939163eaf9abe81148988508505a259745d1a0ac
Brandon Nguyen <rmpumaz@gmail.com>
2021-11-27 17:15:21 -0500

    created players, and winning states array for gameboard

---
+++ script.js
@@ -0,0 +1,14 @@
+//Create Players for Game
+const playerOne = "x";
+const playerTwo = "o";
+
+//Create Winning Conditions for Board // Each array represents a row on the board
+const winningStates = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
